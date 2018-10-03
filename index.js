5dafe88c619caac06d213e713b75078581da1dbf
Progoogler <andyzanli@gmail.com>
2018-10-03 12:49:17 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { 
+  AppRegistry, 
+  // Text,
+} from 'react-native';
+import App from './src/App';
+// Text.defaultProps.allowFontScaling = false;
+
+AppRegistry.registerComponent('LinkMethod', () => App);
